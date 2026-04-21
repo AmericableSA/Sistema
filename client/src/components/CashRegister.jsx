b2a8457ef3e9335a524dcb59cb31efa82ba19e1a
waskar <waskareliasrios11@gmail.com>
2026-04-20 23:39:32 -0600

    cambios

--- client/src/components/CashRegister.jsx
+++ client/src/components/CashRegister.jsx
@@ -448,7 +448,6 @@ const CashRegister = (props) => {
                         filter: brightness(1.15);
                         box-shadow: 0 12px 32px rgba(0,0,0,0.3) !important;
                     }
-                <style>{`
                     .dashboard-action-card {
                         text-align: center;
                         position: relative;
