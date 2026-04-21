acec8107bd3463b3a630f8bb2c9454cee5ba0f3e
waskar <waskareliasrios11@gmail.com>
2026-04-20 23:16:44 -0600

    cambios

--- client/src/pages/Clients.jsx
+++ client/src/pages/Clients.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import ClientModal from '../components/ClientModal';
 import CustomAlert from '../components/CustomAlert';
 import ConfirmModal from '../components/ConfirmModal';
